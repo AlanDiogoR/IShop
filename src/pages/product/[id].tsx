4c3c1a15053d2d25693ce96a42ff878dc646dd01
AlanDiogoR <alandiogor@gmail.com>
2023-01-06 18:31:30 -0300

    página produto terminada

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,82 @@
+import { GetStaticPaths, GetStaticProps } from 'next';
+import Image from 'next/image';
 import { useRouter } from 'next/router';
+import Stripe from 'stripe';
+import { stripe } from '../../lib/stripe';
+import { ImageContainer, ProductContainer, ProductDetails } from '../../styles/pages/product';
 
-export default function Product() {
-  const { query } = useRouter();
+interface ProducProps {
+  product: {
+    id: string,
+    name: string,
+    imageUrl: string,
+    price: string,
+    description: string,
+  }
+}
+
+export default function Product( { product }:ProducProps) {
+  const { isFallback } = useRouter();
+
+  if (isFallback) {
+    return <p>loading</p>;
+  }
 
   return (
-    <h1>Product: {JSON.stringify(query)}</h1>
+    <ProductContainer>
+      <ImageContainer>
+        <Image
+          src={product.imageUrl}
+          width={520}
+          height={480}
+          alt=''
+        />
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
+        <p>{product.description}</p>
+
+        <button>
+          Comprar agora
+        </button>
+      </ProductDetails>
+    </ProductContainer>
   );
 }
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [
+      { params: { id: 'prod_N7U6WPD5Ddf06H'}}
+    ],
+    fallback: true,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const productId = String(params.id);
+
+  const product = await stripe.products.retrieve(productId, {
+    expand: ['default_price'],
+  });
+
+  const price = product.default_price as Stripe.Price;
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        price: new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        }).format(price.unit_amount / 100),
+        description: product.description,
+      }
+    },
+    revalidate: 60 * 60 * 1,
+  };
+};
