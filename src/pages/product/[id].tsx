e3065cb42ad47b697964a0969aab7c725125ae42
AlanDiogoR <alandiogor@gmail.com>
2023-01-09 19:52:28 -0300

    versão rodando

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -2,6 +2,8 @@ import { GetStaticPaths, GetStaticProps } from 'next';
 import axios from 'axios';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
+import { RouterEvent } from 'next/router';
+
 import Stripe from 'stripe';
 import { stripe } from '../../lib/stripe';
 import { ImageContainer, ProductContainer, ProductDetails } from '../../styles/pages/product';
